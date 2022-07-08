9dec44c7ca02d099e24d2b4db40e230ddca89d99
Rajveer Rathod <64583161+rajveer43@users.noreply.github.com>
2022-07-08 20:28:56 +0530

    login

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { registerUser, loginUser, logoutUser, authChecker } = require("../controller/AuthController");
+const { registerLimiter, loginLimiter } = require("../utils/rateLimiter");
+
+// Registers a new User
+router.post("/register", registerLimiter, registerUser );
+
+// Logs In a User, creates session in mongo store
+// and returns a cookie containing sessionID, also called "session-id"
+router.post("/login", loginLimiter, loginUser );
+
+// Log out user by deleting session from store
+// and deleting cookie on client side
+// Needs cookie containing sessionID to be attached to request
+router.delete("/logout", logoutUser );
+
+// Check if user is Authenticated by reading session data
+// Needs cookie containing sessionID
+router.get("/authchecker", authChecker );
+
+module.exports = router;
