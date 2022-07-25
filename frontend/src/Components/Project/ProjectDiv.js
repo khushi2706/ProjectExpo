cff058075552909ad57584317c8ac2f99968d9ba
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-07-25 19:36:04 +0530

    Project div

--- frontend/src/Components/Project/ProjectDiv.js
+++ frontend/src/Components/Project/ProjectDiv.js
@@ -1,35 +1,42 @@
-import React from 'react'
-import './CSS/ProjectDiv.css'
+import React from "react";
+import "./CSS/ProjectDiv.css";
 
 export default function ProjectDiv() {
   return (
-    <div>
-     <div className='col row'>
-      <div className='col-8'>
-      <p className='titile' >Attendance management system using machine learning</p>
+    <div className="main-div">
+      <div className="col row">
+        <div className="col-8">
+          <p className="title">
+            Attendance management system using machine learning
+          </p>
+        </div>
+        <div className="col-4">
+          <span className="tag clg-Tag">BVM</span>
+          <span>4.3k</span>
+          <span>
+            {" "}
+            <i class="fa-solid fa-thumbs-up"></i>
+          </span>
+        </div>
       </div>
-     <div className='col-4'>
-     <span>BVM</span>
-     <span>4.3k</span>
-     <span> <i className="fa fa-like"></i> </span>
-     </div>
-     
-     </div>
-      <p>There is an intrinsic positive relationship between classattendance and the performance of students in the academicenvironment. For the learning to occur more naturally, itis necessary
-         to encourage presence and participation...</p>
-    
-    <div className='row'>
-      <div className="col-8">
-        <span>Machine learning</span>
-        <span>Python</span>
+      <div className="text-left">
+        <p className="text-left">
+          There is an intrinsic positive relationship between classattendance
+          and the performance of students in the academicenvironment. For the
+          learning to occur more naturally, itis necessary to encourage presence
+          and participation...
+        </p>
       </div>
-      <div className="col-4">
-        <span>Author</span>
-        <span>Khushi Patel</span>
+      <div className="row">
+        <div className="col-8">
+          <span className="tag lan-Tag">Machine learning</span>
+          <span className="tag lan-Tag">Python</span>
+        </div>
+        <div className="col-4">
+          <span className="title">Author </span>
+          <span className="tag auth-Tag">Khushi Patel</span>
+        </div>
       </div>
     </div>
-    
-    </div>
-  )
+  );
 }
-
