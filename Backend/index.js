05feeac9dfc9a0fcc57716c21a54852664991047
Dharmesh Vala <dvala453@gmail.com>
2022-08-14 20:21:03 +0530

    subject final done

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const SubjectRoute = require("./routes/subjectRoute");
 const ProfessorRoute = require("./routes/professorRoute");
 const StudentRoute = require("./routes/studentRoute");
 const universityRoute = require("./routes/universityRoute");
-const uploadRoute = require("./controller/uploadProfileController");
+// const uploadRoute = require("./controller/uploadProfileController");
 //create the app
 const app = express();
 
@@ -36,7 +36,7 @@ app.use('/api/subject',SubjectRoute);
 app.use('/api/professor', ProfessorRoute);
 app.use('/api/student',StudentRoute);
 app.use('/api/university',universityRoute);
-app.use('/collegeprofile',uploadRoute);
+// app.use('/collegeprofile',uploadRoute);
 //define port
 
 app.listen(5000, () => console.log("app started at 5000..."));
