4d7bf78ce8136c594905e31cde8d5fecef34aaa6
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-08-14 20:57:08 +0530

    updated api

--- Backend/model/Student.js
+++ Backend/model/Student.js
@@ -31,7 +31,11 @@ UserId:{
     type: mongoose.Types.ObjectId,
     ref:"User",
     required: true
-}
+},
+ProfileImg:{
+    type: String,
+    default : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+},
 })
 
 module.exports = mongoose.model("student", studentSchema);
