325c161d73619358f2db285df2e344d5c627aab2
AlienForOtherPlanets <AlienForotherPlanets@users.noreply.github.com>
2022-08-14 10:55:21 +0530

    Update ProjectDiv.js

--- frontend/src/Components/Project/ProjectDiv.js
+++ frontend/src/Components/Project/ProjectDiv.js
@@ -4,22 +4,22 @@ import "./CSS/ProjectDiv.css";
 
 export default function ProjectDiv(props) {
   const project = props.project
-  const tags =  project.languagesTag
-  console.log(typeof(tags));
+  const tags = project.languagesTag
+  // console.log(typeof (tags));
   return (
     <div className="main-div mt-3">
       <div className="col row">
         <div className="col-8">
-          <Link to={{ pathname:`/viewProject/${project._id}` }}>
-            
-          <p className="title"> {project.title}</p>
-           
-            </Link>  
-      
+          <Link to={{ pathname: `/viewProject/${project._id}` }}>
+
+            <p className="title"> {project.Title}</p>
+
+          </Link>
+
         </div>
         <div className="col-4">
-          <span className="tag clg-Tag">{ project.college }</span>
-          <span>{ project.likes }</span>
+          <span className="tag clg-Tag">{project.University}</span>
+          <span>{project.likes}</span>
           <span>
             {" "}
             <i className="fa-solid fa-thumbs-up"></i>
@@ -28,20 +28,20 @@ export default function ProjectDiv(props) {
       </div>
       <div className="text-left">
         <p className="text-left">
-        {project.desc}
+          {project.Desc}
         </p>
       </div>
       <div className="row">
         <div className="col-8 d-flex  justify-content-left">
-          { 
-          project.languagesTag.map((element, index) => {
-          return <span className="tag lan-Tag"> {element} </span>
-          })
-           }
+          {
+            project.languagesTag.map((element, index) => {
+              return <span className="tag lan-Tag"> {element} </span>
+            })
+          }
         </div>
         <div className="col-4">
           <span className="title">Author </span>
-          <span className="tag auth-Tag">{ project.author }</span>
+          <span className="tag auth-Tag">{project.Author}</span>
         </div>
       </div>
     </div>
