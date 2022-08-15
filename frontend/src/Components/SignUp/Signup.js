162d7100f46e1ec3f8e5db47646548a4efb900c1
Tokir-Powerpbox <tokir77799@gmail.com>
2022-08-15 22:31:47 +0530

    Sign up student page edited  by The Tokir khokhar

--- frontend/src/Components/SignUp/Signup.js
+++ frontend/src/Components/SignUp/Signup.js
@@ -11,14 +11,14 @@ export default function Signup(props) {
   const [departs,setDeparts] = useState([]); 
   const [colleges, setColleges] = useState([]);
   const [ formValue , setformValue ] = useState({
-    Email : "",
-    Pass: "",
-    Fname: "",
-    LName: "",
-    AboutMe: "",
-    DoB: "",
-    Gender:"",
-    DepartmentId:"",
+   // Email : "",
+    // Pass: "",
+    // Fname: "",
+    // LName: "",
+    // AboutMe: "",
+    // DoB: "",
+    // Gender:"",
+    // DepartmentId:"",
   })
 
   const sendReq = async () => {
@@ -50,18 +50,19 @@ export default function Signup(props) {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formValue);
+    console.log("khushliiii"+formValue);
     sendRequest()
       .then((data) => console.log(data))
       .then(() => console.log("done"));
   };
 
 const getDeprtId = async(e) =>{
+    console.log("depart id ")
     const res = await axios
     .get(`http://localhost:5000/api/department/getByCollgeId/${e.target.value}`)
     .catch((err) => console.log(err));
     const data = await res.data;
-    console.log(data);
+    // console.log("data.depatments"+JSON.parse(res["data"]));
     setDeparts( await data.departments);
   }
 
@@ -74,45 +75,119 @@ useEffect(() => {
   console.log(colleges);
   return(
    <>
-   <h1>form</h1>
-
-    <form onSubmit={handleSubmit}>
-    <label htmlFor="">Email</label>
-    <input type="email" value={formValue.Email} />
-    <label htmlFor="">Password</label>
-    <input type="password" value={formValue.Pass} id="" />
-    <label htmlFor="" >First Name</label>
-    <input type="text" value={formValue.Fname} />
-    <label htmlFor="" >Last Name</label>
-    <input type="text" value={formValue.LName} />
-    <label htmlFor="">About Me</label>
-    <input type="text" value={formValue.AboutMe} />
-    <label htmlFor="">Date of Birth</label>
-    <input type="date" name="" value={formValue.DoB} id="" />
-    <label htmlFor="">Gender</label>
-    <input type="text" value={formValue.Gender}/>
-    <label htmlFor="">College</label>
-    <select name="" id="" onChange={getDeprtId}>
-      {
-        colleges && 
-        colleges.map((clg,idx)=>{
-          return(
-      <option value={clg._id}>{clg.CName}</option>
-          )
-        })
-      }
-    </select>
-    <select name="" class="form-control" id="">
-      {
-        departs && 
-        departs.map((dp,idx)=>{
-          return(
-      <option value={dp._id}>{dp.DepartName}</option>
-          )
-        })
-      }
-    </select>
+    <div
+        style={{
+          fontSize: 36,
+          fontWeight: "bold",
+          fontFamily: '"Poppins"',
+          textAlign: "center",
+          marginTop: 38,
+        }}
+      >
+        Sign Up
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+       
+    <form onSubmit={handleSubmit} style={{
+            background: "#F6F7FB",
+            borderRadius: 10,
+            paddingLeft: 68,
+            paddingRight: 68,
+            paddingTop: 61,
+            paddingBottom: 61,
+            width: "40%",
+            marginTop: 25,
+          }}>
+        <div className="box" style={{width: "100%",display: "block"}}>
+          <label htmlFor="">Email</label>
+          <input type="email" value={formValue.Email} style={{width: "100%",}} />
+          <label htmlFor="">Password</label>
+          <input type="password" value={formValue.Pass} id=""  style={{width: "100%"}}/>
+          <label htmlFor="" >First Name</label>
+          <input type="text" value={formValue.Fname}  style={{width: "100%"}}/>
+          <label htmlFor="" >Last Name</label>
+          <input type="text" value={formValue.LName}  style={{width: "100%"}}/>
+          <label htmlFor="">About Me</label>
+          <input type="text" value={formValue.AboutMe}  style={{width: "100%"}}/>
+          <label htmlFor="">Date of Birth</label>
+          <input type="date" name="" value={formValue.DoB} id=""  style={{width: "100%"}}/>
+          <label htmlFor="">Gender</label>
+          <input type="text" value={formValue.Gender} style={{width: "100%"}}/>
+          
+        </div>
+        <div className="box" style={{width: "100%",display: "block"}}>
+          <select name="" id="form" class="form-control" onClick={getDeprtId} style={{margin: "10px 0"}}>
+          <option value=" " >------select college ------</option>
+            {
+              colleges && 
+              colleges.map((clg,idx)=>{
+                return(
+                  <option value={clg._id} >{clg.CName}</option>
+                )
+              })
+            }
+          </select>
+          <select name="" class="form-control" id="" style={{margin: "10px 0"}}>
+            {
+              departs && 
+              departs.map((dp,idx)=>{
+                return(
+            <option value={dp._id}>{dp.DepartName}</option>
+            )
+          })
+        }
+          </select>
+        </div>    
+        <div
+            style={{
+              textAlign: "center",
+              marginTop: 25,
+              fontFamily: "poppins",
+              fontWeight: 800,
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <div>Already have an account ?</div>
+            <div
+              style={{ color: "#2C5EFF", fontWeight: "bolder", marginLeft: 5 }}
+            >
+              Login in
+            </div>
+        </div>
+        <div style={{ textAlign: "center" }}>
+            <button
+              onClick={handleSubmit}
+              className="sign-in-button"
+              style={{ width: "80%", height: "6vh", marginTop: 17 }}
+            >
+              Sign In
+            </button>
+        </div>
+        <div style={{ textAlign: "center" }}>
+            <button
+              className="sign-in-with-google-button"
+              style={{ height: "6vh" }}
+            >
+              <div style={{ display: "flex" }}>
+                <div style={{ margin: "auto" }}>
+                  <img src={google_logo} alt="" width="70%" height="70%" />
+                </div>
+                <div
+                  style={{
+                    fontFamily: "poppins",
+                    fontWeight: "bold",
+                    margin: "auto",
+                  }}
+                >
+                  Sign in with Google
+                </div>
+              </div>
+            </button>
+        </div>
+         
     </form>
+    </div>
    </> 
   )
 }
