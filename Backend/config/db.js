5121e8201af5d83288d1b10423b5eeb13081487f
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-08-16 22:15:47 +0530

    change details api done

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/ProjectExpo").then(()=>{
     console.log("Database is connected!");
 }).catch((err)=>{
-    console.log("error");
+    console.log(err);
 })
