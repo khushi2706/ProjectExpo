6dad6ce9a44d074acf11e353b7545aa09ff2a9ed
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-08-18 20:53:46 +0530

    sign up merged

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -73,5 +73,36 @@ const loginUser = async (req,res,next) =>{
     
   
 }
+const addNewUser = async (req, res, next) => {
+  const { Email, Password, UserType } = req.body;
+  console.log("API->Email: " + Email);
+  try {
+    let existingUser = await User.findOne({ Email: Email });
+    console.log("we are inside the try block");
+    if (existingUser)
+      return res.status(400).json({
+        success: false,
+        message: "User Already exists",
+      });
+    else {
+      console.log("We are about to create a new user");
+      const user = new User({
+        Email,
+        Password,
+        UserType,
+        // UserId,
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      response: {
+        message: error.message,
+      },
+    });
+  }
+};
+
+
 
 module.exports = { getAllUser, loginUser, addNewUser };
