71fd0190442c3a668cac8b784f4dba495c4dddf9
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-08-19 23:31:57 +0530

    Subejct id aip

--- Backend/routes/projectRoute.js
+++ Backend/routes/projectRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const projectRoute = express.Router();
 
-const { getAllProjects , getProjectById , getProjectByUserId ,addNewProject ,  } = require("../controller/projectController");
+const { getAllProjects , getProjectById , getProjectByUserId ,addNewProject , getProjectBySubjectID } = require("../controller/projectController");
 
 projectRoute.get("/",getAllProjects);
 projectRoute.get("/:id",getProjectById);
 projectRoute.get("/UserId/:UserId",getProjectByUserId);
 projectRoute.post("/add" , addNewProject);
+projectRoute.get("/Subject/:SubjectId", getProjectBySubjectID );
 
 module.exports = projectRoute;
