6e258679a09ba8b7fa7a3af7d680dd32d3ad6e70
Dharmesh Vala <dvala453@gmail.com>
2022-08-23 09:12:32 +0530

    dynamic university

--- frontend/src/Components/University/University.js
+++ frontend/src/Components/University/University.js
@@ -1,132 +1,103 @@
 import "./University.css";
+import React, { useState, useEffect } from "react";
 import Search from "../Home/Search/Search";
 import Header from "../Common/Header";
 import RecentSearchTag from "./RecentSearchTag";
 import UniversityCard from "./UniversityCard";
+import axios from "axios";
+
 export default function University() {
+  const [user, setUser] = useState();
+
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/university`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    console.log(data);
+    console.log("-----");
+    return data;
+  };
+
+  useEffect(() => {
+    sendRequest().then((data) => setUser(data.universities));
+
+    console.log(user);
+  }, []);
   return (
     <>
-      <div style={{ position: "sticky" }}>
-        <Header />
-      </div>
+      {user && (
+        <>
+          <div style={{ position: "sticky" }}>
+            <Header />
+          </div>
 
-      <div style={{ position: "sticky" }}>
-        <Search title="Search University" />
-      </div>
+          <div style={{ position: "sticky" }}>
+            <Search title="Search University" />
+          </div>
 
-      <div
-        style={{
-          position: "sticky",
-          textAlign: "left",
-          fontSize: 22,
-          fontWeight: "bold",
-          marginLeft: 20,
-          marginTop: 20,
-          fontFamily: "poppins",
-        }}
-      >
-        Recent Search
-      </div>
+          <div
+            style={{
+              position: "sticky",
+              textAlign: "left",
+              fontSize: 22,
+              fontWeight: "bold",
+              marginLeft: 20,
+              marginTop: 20,
+              fontFamily: "poppins",
+            }}
+          >
+            Recent Search
+          </div>
 
-      <div
-        style={{
-          position: "sticky",
-          display: "flex",
-          flexDirection: "row",
-          marginTop: 10,
-        }}
-      >
-        <RecentSearchTag title="Birla Vishwakarma Mahavidhyalaya" />
-        <RecentSearchTag title="DDU" />
-        <RecentSearchTag title="Nirma" />
-        <RecentSearchTag title="IIT Mumbai" />
-      </div>
+          <div
+            style={{
+              position: "sticky",
+              display: "flex",
+              flexDirection: "row",
+              marginTop: 10,
+            }}
+          >
+            <RecentSearchTag title="Birla Vishwakarma Mahavidhyalaya" />
+            <RecentSearchTag title="DDU" />
+            <RecentSearchTag title="Nirma" />
+            <RecentSearchTag title="IIT Mumbai" />
+          </div>
 
-      <hr></hr>
+          <hr></hr>
 
-      <div style={{ display: "flex", marginBottom: 20 }}>
-        <div
-          style={{
-            position: "sticky",
-            display: "flex",
-            flexDirection: "row",
-            top: 200,
-            flexWrap: "wrap",
-          }}
-        >
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-
-          <UniversityCard
-            title="Birla Vishwakarma Mahavidhyalaya"
-            information="Birla Vishvakarma Mahavidyalaya Engineering College was established in 1948 from donations made by the Birla Education Trust on the behest of Sardar Vallabhbhai Patel, the first Home Minister of independent India. The college was inaugurated by Lord Mountbatten, the Governor General of India on 14 June 1948, and rose to prominence under the stewardship of Prof. Junarkar and Prof. K.M. Dholakia. It was one of the first few colleges in India that adopted the progressive credit system of relative grading in India. The college has awarded degrees to over 17,000 graduates, and has its alumni spread across the globe."
-            universityType="Government"
-          />
-        </div>
-      </div>
+         
+            <div
+              style={{
+                position: "sticky",
+                display: "flex",
+                flexDirection: "row",
+                top: 200,
+                flexWrap: "wrap",
+              }}
+            >
+            <div className="container">
+            <div className="row" style={{ marginBottom: 20 }}>
+           
+            
+            {user.map((data, index) => (
+              <div className="col-md-3">
+              <UniversityCard
+                title={data.UName}
+                information={data.Address}
+                universityType={data.UType}
+                email={data.Email}
+                district={data.District}
+                key={index}
+              />
+              </div>
+            ))}
+            </div>
+            </div>
+            </div>
+          
+        </>
+      )}
     </>
   );
 }
