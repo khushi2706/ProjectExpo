09bd83b82f8eafc067ce7131b74abc18bcf2ee9d
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2022-08-26 12:38:21 +0530

    cmo

--- Backend/routes/studentRoute.js
+++ Backend/routes/studentRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const StudentRoute = express.Router();
 
-const {  getAllStudent , addNewStudent ,getAllStudentByDepartId , getStudentById,changeStudentDetails } = require("../controller/StudentController");
+const {  getAllStudent , addNewStudent ,getAllStudentByDepartId , getAllStudentByUserId, getStudentById,changeStudentDetails } = require("../controller/StudentController");
 const Student = require("../model/Student");
 
 StudentRoute.get("/",getAllStudent);
@@ -9,5 +9,5 @@ StudentRoute.post("/add",addNewStudent);
 StudentRoute.get("/getByDepartId/:departId",getAllStudentByDepartId);
 StudentRoute.put("/changeDetails",changeStudentDetails);
 StudentRoute.get("/getStudentById/:id",getStudentById);
-
+StudentRoute.get("/getByUserId/:userId",getAllStudentByUserId);
 module.exports = StudentRoute;
